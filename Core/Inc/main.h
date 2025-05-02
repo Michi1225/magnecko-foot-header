c9ce743a7f899d31e893d88ab6c487736e84db5f
Michael Haslinger <mhaslinger@ethz.ch>
2025-05-02 16:21:51 +0200

    STM32 Pinout defined

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,62 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define NFLT_Pin GPIO_PIN_0
+#define NFLT_GPIO_Port GPIOH
+#define DRV_M_Pin GPIO_PIN_1
+#define DRV_M_GPIO_Port GPIOH
+#define DRV_P_Pin GPIO_PIN_0
+#define DRV_P_GPIO_Port GPIOC
+#define CHARGE_START_Pin GPIO_PIN_1
+#define CHARGE_START_GPIO_Port GPIOC
+#define CHARGE_DONE_Pin GPIO_PIN_0
+#define CHARGE_DONE_GPIO_Port GPIOA
+#define IMEAS_Pin GPIO_PIN_1
+#define IMEAS_GPIO_Port GPIOA
+#define EEPROM_LOADED_Pin GPIO_PIN_2
+#define EEPROM_LOADED_GPIO_Port GPIOA
+#define ECAT_IRQ_Pin GPIO_PIN_3
+#define ECAT_IRQ_GPIO_Port GPIOA
+#define ECAT_NCS_Pin GPIO_PIN_4
+#define ECAT_NCS_GPIO_Port GPIOA
+#define ECAT_MISO_Pin GPIO_PIN_5
+#define ECAT_MISO_GPIO_Port GPIOA
+#define ECAT_MISOA6_Pin GPIO_PIN_6
+#define ECAT_MISOA6_GPIO_Port GPIOA
+#define ECAT_MOSI_Pin GPIO_PIN_7
+#define ECAT_MOSI_GPIO_Port GPIOA
+#define SYNC0_Pin GPIO_PIN_4
+#define SYNC0_GPIO_Port GPIOC
+#define SYNC1_Pin GPIO_PIN_5
+#define SYNC1_GPIO_Port GPIOC
+#define TOF_INT_Pin GPIO_PIN_6
+#define TOF_INT_GPIO_Port GPIOC
+#define TOF_I2C_RST_Pin GPIO_PIN_7
+#define TOF_I2C_RST_GPIO_Port GPIOC
+#define TOF_LP_Pin GPIO_PIN_9
+#define TOF_LP_GPIO_Port GPIOA
+#define LDC_INT_Pin GPIO_PIN_10
+#define LDC_INT_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define IMU_NCS_Pin GPIO_PIN_15
+#define IMU_NCS_GPIO_Port GPIOA
+#define IMU_SCK_Pin GPIO_PIN_12
+#define IMU_SCK_GPIO_Port GPIOC
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define IMU_MISO_Pin GPIO_PIN_4
+#define IMU_MISO_GPIO_Port GPIOB
+#define IMU_MOSI_Pin GPIO_PIN_5
+#define IMU_MOSI_GPIO_Port GPIOB
+#define IMU_NRST_Pin GPIO_PIN_7
+#define IMU_NRST_GPIO_Port GPIOB
+#define IMU_WAKE_Pin GPIO_PIN_8
+#define IMU_WAKE_GPIO_Port GPIOB
+#define IMU_BOOTN_Pin GPIO_PIN_9
+#define IMU_BOOTN_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
---
+++ Core/Src/gpio.c
@@ -0,0 +1,121 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DRV_M_GPIO_Port, DRV_M_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, DRV_P_Pin|CHARGE_START_Pin|TOF_I2C_RST_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, ECAT_NCS_Pin|TOF_LP_Pin|IMU_NCS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, IMU_NRST_Pin|IMU_WAKE_Pin|IMU_BOOTN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : NFLT_Pin */
+  GPIO_InitStruct.Pin = NFLT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(NFLT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DRV_M_Pin */
+  GPIO_InitStruct.Pin = DRV_M_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DRV_M_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DRV_P_Pin CHARGE_START_Pin TOF_I2C_RST_Pin */
+  GPIO_InitStruct.Pin = DRV_P_Pin|CHARGE_START_Pin|TOF_I2C_RST_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : CHARGE_DONE_Pin EEPROM_LOADED_Pin */
+  GPIO_InitStruct.Pin = CHARGE_DONE_Pin|EEPROM_LOADED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ECAT_IRQ_Pin LDC_INT_Pin */
+  GPIO_InitStruct.Pin = ECAT_IRQ_Pin|LDC_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ECAT_NCS_Pin TOF_LP_Pin IMU_NCS_Pin */
+  GPIO_InitStruct.Pin = ECAT_NCS_Pin|TOF_LP_Pin|IMU_NCS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SYNC0_Pin SYNC1_Pin TOF_INT_Pin */
+  GPIO_InitStruct.Pin = SYNC0_Pin|SYNC1_Pin|TOF_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : IMU_NRST_Pin IMU_WAKE_Pin IMU_BOOTN_Pin */
+  GPIO_InitStruct.Pin = IMU_NRST_Pin|IMU_WAKE_Pin|IMU_BOOTN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
