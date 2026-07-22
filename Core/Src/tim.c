a502fa1c43f324e4ee39a88af398e48ed854c51e
Michael Haslinger <mhaslinger@ethz.ch>
2026-07-22 11:04:20 +0200

    Corrected One-Pulse Timer Frequency

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -48,7 +48,7 @@ void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 550-1;
+  htim1.Init.Prescaler = 275-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 20000;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -166,7 +166,7 @@ void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 550-1;
+  htim3.Init.Prescaler = 275-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 20000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
