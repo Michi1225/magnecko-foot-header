e77369cb9f0012af345e0e46f4fcded583ba2233
Michael Haslinger <mhasinger@ethz.ch>
2026-09-08 16:17:30 +0200

    Added Button Pullup

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -187,7 +187,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : BUTTON_Pin */
   GPIO_InitStruct.Pin = BUTTON_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(BUTTON_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : MAG_STAT_Pin */
