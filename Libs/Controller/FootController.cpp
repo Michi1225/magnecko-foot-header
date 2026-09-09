ccf5c339ee03b197ab52eaedc1e475f0c5a40b12
Michael Haslinger <mhaslinger@ethz.ch>
2026-09-09 18:33:45 +0200

    Skipping Cap Charger initialization if Batteries are used.

--- Libs/Controller/FootController.cpp
+++ Libs/Controller/FootController.cpp
@@ -141,11 +141,13 @@ void FootController::init()
         this->controller_error_word.tof_init_failed = 1;
     }
 
+#ifndef USE_BATTERIES
     // Charger Initialization
     if(!charger.wait_ready(1000)) 
     {
         this->controller_error_word.charger_init_failed = 1;
     }
+#endif
 
 
 
